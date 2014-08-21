3799c1dcdea4a99c12c9bafd7ef78aa31a108465
0r4cl3 <lucalicheri@gmail.com>
2014-08-21 23:05:18 +0100

    validation for email + test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,25 @@
 require 'rails_helper'
 
-describe User do
+	describe User do
 
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com")
-  end
+	before do
+		@user = User.new(name: "Example User", email: "user@example.com")
+	end
 
-  subject { @user }
+	subject { @user }
 
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
+	it { should respond_to(:name) }
+	it { should respond_to(:email) }
 
-  it { should be_valid }
+	it { should be_valid }
 
-  describe "when name is not present" do
-    before { @user.name = " " }
-    it { should_not be_valid }
-  end
+	describe "when name is not present" do
+		before { @user.name = " " }
+		it { should_not be_valid }
+	end
+
+	describe "when email is not present" do
+		before { @user.email = " " }
+		it { should_not be_valid }
+	end
 end
