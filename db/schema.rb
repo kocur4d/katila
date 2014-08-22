5f7a43e9f710b209b670b678991af37371f2d50a
Luca Licheri <lucalicheri@gmail.com>
2014-08-22 15:17:45 +0100

    Add index to users email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821213836) do
+ActiveRecord::Schema.define(version: 20140822141701) do
 
   create_table "users", force: true do |t|
     t.string   "name"
