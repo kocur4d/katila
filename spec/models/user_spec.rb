3e17206586cef7a777dc929e3e1075b15cc71aeb
Luca Licheri <lucalicheri@gmail.com>
2014-08-22 18:45:00 +0100

    Add has secure password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 	describe User do
 
 	before do
-		@user = User.new(name: "Example User", email: "user@example.com")
+		@user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
 	end
 
 	subject { @user }
@@ -13,6 +13,19 @@ require 'rails_helper'
 	it { should respond_to(:password_digest) }
 
 	it { should be_valid }
+
+	describe "when password is not present" do
+		before do
+			@user = User.new(name: "Example User", email: "user@example.com",
+			password: " ", password_confirmation: " ")
+		end
+		it { should_not be_valid }
+	end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
 
 	describe "when name is not present" do
 		before { @user.name = " " }
